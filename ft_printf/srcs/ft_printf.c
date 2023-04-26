a1e41af1db436ac7794b5582df31d07471243d7c
jaeheele <sns3434@naver.com>
2023-04-26 23:58:14 +0900

    rename variables

--- ft_printf/srcs/ft_printf.c
+++ ft_printf/srcs/ft_printf.c
@@ -6,57 +6,59 @@
 /*   By: jaeheele <jaeheele@student.42seoul.kr>     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/03/26 00:20:39 by jaeheele          #+#    #+#             */
-/*   Updated: 2023/04/25 21:48:31 by jaeheele         ###   ########.fr       */
+/*   Updated: 2023/04/26 23:43:18 by jaeheele         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "ft_printf.h"
 
-static int	ft_print_arg(const char **format, va_list args)
+static int	ft_print_arg(const char **format, va_list ap)
 {
-	int	s_len;
+	int	rslt;
 
-	s_len = 0;
+	rslt = 0;
 	if (**format == 's')
-		s_len += ft_putstr(va_arg(args, char *));
+		rslt += ft_putstr(va_arg(ap, char *));
 	else if (**format == 'd' || **format == 'i')
-		s_len += ft_putnbr(va_arg(args, int));
+		rslt += ft_putnbr(va_arg(ap, int));
 	else if (**format == 'u')
-		s_len += ft_putunbr(va_arg(args, unsigned int));
+		rslt += ft_putunbr(va_arg(ap, unsigned int));
 	else if (**format == 'x' || **format == 'X')
-		s_len += ft_puthex(va_arg(args, unsigned int), **format);
+		rslt += ft_puthex(va_arg(ap, unsigned int), **format);
 	else if (**format == 'c')
-		s_len += ft_putchar(va_arg(args, int));
+		rslt += ft_putchar(va_arg(ap, int));
 	else if (**format == 'p')
 	{
-		s_len += ft_putstr("0x");
-		s_len += ft_puthex((unsigned long long)va_arg(args, void *), 'x');
+		rslt += ft_putstr("0x");
+		rslt += ft_puthex((unsigned long long)va_arg(ap, void *), 'x');
 	}
 	else if (**format == '%')
-		s_len += ft_putchar('%');
+		rslt += ft_putchar('%');
 	else
-		s_len = -1;
+		rslt = -1;
 	(*format)++;
-	return (s_len);
+	return (rslt);
 }
 
 int	ft_printf(const char *format, ...)
 {
-	va_list	args;
-	int		s_len;
+	va_list	ap;
+	int		rslt;
 
-	s_len = 0;
-	va_start(args, format);
+	if (!(format))
+		return (-1);
+	rslt = 0;
+	va_start(ap, format);
 	while (*format)
 	{
 		if (*format == '%')
 		{
 			format++;
-			s_len += ft_print_arg(&format, args);
+			rslt += ft_print_arg(&format, ap);
 		}
 		else
-			s_len += write(1, format++, 1);
+			rslt += write(1, format++, 1);
 	}
-	va_end(args);
-	return (s_len);
+	va_end(ap);
+	return (rslt);
 }
--- ft_printf/srcs/ft_printf_utils.c
+++ ft_printf/srcs/ft_printf_utils.c
@@ -6,7 +6,7 @@
 /*   By: jaeheele <jaeheele@student.42seoul.kr>     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/03/26 00:21:43 by jaeheele          #+#    #+#             */
-/*   Updated: 2023/03/26 07:54:48 by jaeheele         ###   ########.fr       */
+/*   Updated: 2023/04/26 23:44:48 by jaeheele         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -20,71 +20,70 @@ int	ft_putchar(char c)
 int	ft_putstr(char *str)
 {
 	int	i;
-	int	s_len;
+	int	len;
 
 	i = 0;
-	s_len = 0;
+	len = 0;
 	if (str == NULL)
 		return (ft_putstr("(null)"));
 	while (str[i] != '\0')
 	{
-		s_len += write(1, &str[i], 1);
+		len += write(1, &str[i], 1);
 		i++;
 	}
-	return (s_len);
+	return (len);
 }
 
 int	ft_putnbr(int nb)
 {
-	int	s_len;
+	int	len;
 
-	s_len = 0;
+	len = 0;
 	if (nb == -2147483648)
-		s_len += write(1, "-2147483648", 11);
+		len += write(1, "-2147483648", 11);
 	else
 	{
 		if (nb < 0)
-			s_len += write(1, "-", 1) + ft_putnbr(nb * (-1));
+			len += write(1, "-", 1) + ft_putnbr(nb * (-1));
 		else
 		{
 			if (nb / 10 == 0)
 			{
 				nb = nb + '0';
-				s_len += write(1, &nb, 1);
+				len += write(1, &nb, 1);
 			}
 			else
 			{
-				s_len += ft_putnbr(nb / 10) \
-				+ ft_putnbr(nb % 10);
+				len += ft_putnbr(nb / 10) + ft_putnbr(nb % 10);
 			}
 		}
 	}
-	return (s_len);
+	return (len);
 }
 
 int	ft_putunbr(unsigned int nb)
 {
-	int	s_len;
+	int	len;
 
-	s_len = 1;
+	len = 1;
 	if (nb / 10 > 0)
-		s_len += ft_putunbr(nb / 10);
+		len += ft_putunbr(nb / 10);
 	ft_putchar(nb % 10 + '0');
-	return (s_len);
+	return (len);
 }
 
 int	ft_puthex(unsigned long long num, char type)
 {
-	int		s_len;
+	int		len;
 	char	*hex;
 
-	s_len = 1;
+	len = 1;
 	if (type == 'x')
 		hex = "0123456789abcdef";
 	else
 		hex = "0123456789ABCDEF";
 	if (num >= 16)
-		s_len += ft_puthex(num / 16, type);
+		len += ft_puthex(num / 16, type);
 	ft_putchar(hex[num % 16]);
-	return (s_len);
+	return (len);
 }
