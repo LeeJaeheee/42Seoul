43f263dac95ea9e245c5504ff467033798358325
jaeheele <sns3434@naver.com>
2023-04-28 01:04:23 +0900

    ft_printf

--- ft_printf/srcs/ft_printf.c
+++ ft_printf/srcs/ft_printf.c
@@ -6,62 +6,11 @@
 /*   By: jaeheele <jaeheele@student.42seoul.kr>     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/03/26 00:20:39 by jaeheele          #+#    #+#             */
-/*   Updated: 2023/04/27 04:23:47 by jaeheele         ###   ########.fr       */
+/*   Updated: 2023/04/28 01:02:33 by jaeheele         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "ft_printf.h"
-
-// static int	ft_print_arg(const char **format, va_list ap)
-// {
-// 	int	rslt;
-
-// 	rslt = 0;
-// 	if (**format == 's')
-// 		rslt += ft_putstr(va_arg(ap, char *));
-// 	else if (**format == 'd' || **format == 'i')
-// 		rslt += ft_putnbr(va_arg(ap, int));
-// 	else if (**format == 'u')
-// 		rslt += ft_putunbr(va_arg(ap, unsigned int));
-// 	else if (**format == 'x' || **format == 'X')
-// 		rslt += ft_puthex(va_arg(ap, unsigned int), **format);
-// 	else if (**format == 'c')
-// 		rslt += ft_putchar(va_arg(ap, int));
-// 	else if (**format == 'p')
-// 	{
-// 		rslt += ft_putstr("0x");
-// 		rslt += ft_puthex((unsigned long long)va_arg(ap, void *), 'x');
-// 	}
-// 	else if (**format == '%')
-// 		rslt += ft_putchar('%');
-// 	else
-// 		rslt = -1;
-// 	(*format)++;
-// 	return (rslt);
-// }
-
-// int	ft_printf(const char *format, ...)
-// {
-// 	va_list	ap;
-// 	int		rslt;
-
-// 	if (!(format))
-// 		return (-1);
-// 	rslt = 0;
-// 	va_start(ap, format);
-// 	while (*format)
-// 	{
-// 		if (*format == '%')
-// 		{
-// 			format++;
-// 			rslt += ft_print_arg(&format, ap);
-// 		}
-// 		else
-// 			rslt += write(1, format++, 1);
-// 	}
-// 	va_end(ap);
-// 	return (rslt);
-// }
 
 static int	ft_print_arg(const char format, va_list ap)
 {
